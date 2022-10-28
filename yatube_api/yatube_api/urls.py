5880ca570abb73e6a0f68355e4414b23fdf8d24a
Терницкий Антон <A60874022@yandex.ru>
2022-10-28 21:02:05 +0500

    9sprint

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
-from django.conf.urls import url
+
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -24,13 +24,4 @@ urlpatterns = [
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc'
-    ),
-]
-urlpatterns += [
-    url(r'^swagger(?P<format>\.json|\.yaml)$',
-        schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
-        name='schema-swagger-ui'),
-    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
-        name='schema-redoc'),
-]
+    )]
